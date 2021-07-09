3b08b29bd411a5d4b07d3e6fc2f8275967e99a1f
Will Ernst <will.ernst15@gmail.com>
2021-07-08 20:17:05 -0700

    basic group/aggregate implementation

--- src/graph-nodes/index.tsx
+++ src/graph-nodes/index.tsx
@@ -9,5 +9,6 @@ export { default as FileSource } from "./nodes/filesource-node";
 export { default as Writer } from "./nodes/writer-node";
 export { default as Spreadsheet } from "./nodes/spreadsheet-node";
 export { default as Join } from "./nodes/join-node";
+export { default as Group } from "./nodes/group-node";
 
 export * from "./nodes/primitives";
