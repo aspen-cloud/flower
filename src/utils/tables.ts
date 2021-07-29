f6b64181bc29fd4322fd174404a5a95f72b05f8d
Will Ernst <will.ernst15@gmail.com>
2021-07-29 08:07:47 -0700

    allow exact character matches and commas in numbers

--- src/utils/tables.ts
+++ src/utils/tables.ts
@@ -1,14 +1,14 @@
 import { nanoid } from "nanoid";
 import { Column, ColumnType, RowValue, Table } from "../types";
-import columnParsers from "../column-parsers";
+import columnParsers, { parseNumber } from "../column-parsers";
 
 export function jsonToTable(json_data: Record<string, any>[]): Table {
-  const columns: Column[] = Object.keys(
+  const columns: Column[] = Object.entries(
     json_data.length ? json_data[0] : {},
-  ).map((col) => ({
+  ).map(([col, value]) => ({
     Header: col,
     accessor: nanoid(),
-    Type: { name: "Text" }, // TODO: hard coded as obj
+    Type: { name: inferType(value) },
   }));
 
   const columnIndex = Object.fromEntries(columns.map((c, i) => [c.Header, c]));
@@ -31,10 +31,14 @@ export function jsonToTable(json_data: Record<string, any>[]): Table {
 export function matrixToTable(matrix_data: string[][]): Table {
   const [colData, ...rowData] = matrix_data;
 
-  const columns = colData.map((col) => ({
+  const types = rowData.length
+    ? rowData[0].map((val) => inferType(val))
+    : new Array(colData.length).fill("Text");
+
+  const columns = colData.map((col, i) => ({
     Header: col,
     accessor: nanoid(),
-    Type: { name: "Text" }, // TODO: hard coded as obj
+    Type: { name: types[i] },
   }));
 
   const rows = rowData.map((row) =>
@@ -55,22 +59,42 @@ export function matrixToTable(matrix_data: string[][]): Table {
 export function parseRow(value: string, typeDef: ColumnType): RowValue {
   const parser = columnParsers[typeDef.name];
   let readValue: string;
+  let writeValue: string;
   let underlyingValue: any;
   let error: Error;
   try {
     const result = parser.parse(value);
     readValue = result.readValue;
+    writeValue = result.writeValue;
     underlyingValue = result.underlyingValue;
   } catch (err) {
     error = err;
     readValue = value;
+    writeValue = value;
     console.log(err);
   }
 
   return {
     readValue,
-    writeValue: value,
+    writeValue,
     underlyingValue,
     error,
   };
+}
+
+export function inferType(val: string) {
+  if (val) {
+    if (val.startsWith("$")) {
+      const parsedNumber = parseNumber(val.substring(1));
+      if (!isNaN(parsedNumber)) return "Currency";
+    } else if (val.endsWith("%")) {
+      const parsedNumber = parseNumber(val.substring(0, val.length - 1));
+      if (!isNaN(parsedNumber)) return "Percentage";
+    }
+
+    const parsedNumber = parseNumber(val);
+    if (!isNaN(parsedNumber)) return "Number";
+  }
+
+  return "Text";
 }
